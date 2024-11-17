e47ca6eb6a7cba4a9a62a705bd04c3b5a80fdc0c
dinakaraDatta <124520362+dinakaraDatta@users.noreply.github.com>
2024-11-16 18:58:30 -0600

    Add files via upload
    updated layout file with the nav bar code added

--- hackutd24/src/app/layout.js
+++ hackutd24/src/app/layout.js
@@ -5,18 +5,39 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
 import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+          <nav>
+            <ul className="navbar">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/services">Services</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <SignedOut>
+                <li>
+                  <SignInButton />
+                </li>
+              </SignedOut>
+              <SignedIn>
+                <li>
+                  <User Button />
+                </li>
+              </SignedIn>
+            </ul>
+          </nav>
           {children}
         </body>
       </html>
